1d8f6e9dd4fa8a8aa673161938c4adac0e1ab0a3
yuvraj2306 <92243712+yuvraj2306@users.noreply.github.com>
2022-05-31 16:09:56 +0530

    Update jenkins_sample_script.sh

--- jenkins_sample_script.sh
+++ jenkins_sample_script.sh
@@ -7,3 +7,4 @@ echo $JOB_NAME
 echo "User has selected Region as $REGION_CHOICE"
 echo "User has selected Env as $ENV_CHOICE"
 echo "User has selected Env as $ENV_CHOICE"
+echo "This is simple for github Webhook test"
